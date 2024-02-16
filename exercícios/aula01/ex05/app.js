91be0849f2bdb3c371d832e54c9a55fe3f7b063f
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-02-16 15:14:29 -0300

    exercício 05 finalizado

---
+++ exercícios/aula01/ex05/app.js
@@ -0,0 +1,6 @@
+function clicar() {
+    let num1 = parseInt(prompt('Digite um número: '));
+    let num2 = parseInt(prompt('Digite outro número: '));
+    let soma = num1 + num2;
+    alert('O resultado da soma é igual a: '+ soma);
+}
