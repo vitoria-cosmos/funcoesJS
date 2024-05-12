d826dd3fd5cf3d4bf40d5c877a6926a4ea07c31c
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-05-12 19:33:18 -0300

    função para sortear o número secreto criada

--- aulas/app.js
+++ aulas/app.js
@@ -4,16 +4,24 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function exibirTextoNaTela (tag, texto) {
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+    campo.innerHTML = texto; 
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
 
-function verificarChute(){
-    console.log('O botão foi clicado!');
+// function verificarChute() {
+//     console.log('O botão foi clicado!');
+// }
 
+function verificarChute() {
+    console.log('O número secreto é:',numeroSecreto);
 }
 
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
